44bc1a15e085725ad6f5345c0cebc33666ccc8b4
Tatiana Krishtop <tkrishto@redhat.com>
2022-10-04 13:42:12 +0200

    Implement parallel calls to speakers

--- pkg/listener/listener.go
+++ pkg/listener/listener.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 )
 
 func callUrl(url string) string {
@@ -13,28 +14,47 @@ func callUrl(url string) string {
 
 	response, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("[listener] There is an error to call url, ignore it", err)
+		return ""
 	}
 	defer response.Body.Close()
 
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("[listener] There is an error convert response data, ignore it", err)
+		return ""
 	}
 
 	return string(responseData)
 }
+
+// func parallelCall(ctx, url, wg) {
+//     ctx, cancel := context.WithTimeout(ctx, time.Second)
+//     defer wg.Done()
+//     defer cancel()
+
+//     callUrl(ctx, url)
+// }
 
 func gatherRequests(w http.ResponseWriter, r *http.Request) {
 	speakers := []string{os.Getenv("WINNIE_URL"), os.Getenv("PIGLET_URL")}
 	ports := []string{os.Getenv("WINNIE_PORT"), os.Getenv("PIGLET_PORT")}
 	log.Println("[listener] Got a list of URLs", speakers)
 
+	// collect replies from all speakers
+	wg := sync.WaitGroup{}
+	wg.Add(len(speakers))
 	concatenatedResponse := ""
-	for idx, urlString := range speakers {
-		concatenatedResponse += callUrl("http://" + urlString + ports[idx])
+
+	for idx, urlBase := range speakers {
+		urlString := "http://" + urlBase + ports[idx]
+		go func(s string) {
+			defer wg.Done()
+			concatenatedResponse += callUrl(s)
+		}(urlString)
 	}
 
+	wg.Wait()
 	w.Header().Set("Content-Type", "text/plain")
 	fmt.Fprintf(w, "Concatenated replies: \n%s \n", concatenatedResponse)
 }
